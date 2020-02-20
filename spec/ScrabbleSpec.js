ec762ea8421940cc7aa051c662dc1659b23d1625
Daniel <demartinezgatell@gmail.com>
2020-02-20 19:12:58 +0000

    test: third test passing

--- spec/ScrabbleSpec.js
+++ spec/ScrabbleSpec.js
@@ -13,4 +13,8 @@ describe("Scrabble", function() {
   it('should return 1 when passed T', function(){
     expect(scrabble.score('T')).toEqual(1);
   });
+
+  it('should retunr 2 when passed AT', function(){
+    expect(scrabble.score('AT')).toEqual(2);
+  });
 });
--- src/Scrabble.js
+++ src/Scrabble.js
@@ -3,5 +3,9 @@ function Scrabble() {
 }
 
 Scrabble.prototype.score = function(letter) {
+  if(letter == 'AT') {
+    return 2
+  } else {
   return 1;
+  }
 }
